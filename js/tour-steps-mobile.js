2defa8a7ad52825c687931917e00042c29eb350d
Dheeraj Arora <dheeraj.a@nopaperforms.com>
2021-04-01 15:02:17 +0530

    changes

---
+++ js/tour-steps-mobile.js
@@ -0,0 +1,88 @@
+const tour = new Tour({
+    name: 'mytourname',
+    backdrop: true,
+    template: "<div class='popover tour mt-1'><div class='arrow'></div><h3 class='popover-title '></h3><div class='popover-content'></div><div class='popover-navigation'><button class='btn cs-btn mt-0 ml-0' data-role='prev'><i class='icon-long-arrow-left'></i></button><button class='btn cs-btn mt-0 ml-0' data-role='next'><i class='icon-long-arrow'></i></button><button class='btn btn-secondary' data-role='end'>Skip</button></div></div>",
+    steps: [{
+            element: "#tour-element-1",
+            title: "Notifications",
+            content: "Recent Notification",
+            placement: "left"
+        }, {
+            element: "#tour-element-2",
+            title: "Dashboard",
+            content: "List for all application",
+            placement: "top"
+        }, {
+            element: "#tour-element-3",
+            title: "Program Finder",
+            content: "List for all application",
+            placement: "top"
+        }, {
+            element: "#tour-element-4",
+            title: "My Applications",
+            content: "List for all application",
+            placement: "top"
+        }, {
+            element: "#tour-element-5",
+            title: "My Queries",
+            content: "List for all application",
+            placement: "top"
+        }, {
+            element: "#tour-element-6",
+            title: "My Communications",
+            content: "List for all application"
+        },
+        {
+            element: "#tour-element-7",
+            title: "My Payment",
+            content: "List for all application"
+        }, {
+            element: "#lastFormCard",
+            title: "Last Active Form",
+            content: "List for all application",
+            placement: "bottom"
+        }, {
+            element: "#applicationCard",
+            title: "My Applications",
+            content: "List for all application",
+            placement: "bottom"
+        },
+        {
+            element: "#programCard",
+            title: "My Program Finder",
+            content: "Find related program",
+            placement: "bottom"
+        },
+        {
+            element: "#queriesCard",
+            title: "My Queries",
+            content: "Any Questions",
+            placement: "top"
+        },
+        {
+            element: "#latestNotification",
+            title: "Latest Notifications",
+            content: "Any Questions",
+            placement: "top",
+        },
+        {
+            element: "#sidebarTour",
+            title: "Menu",
+            content: "Menu for other items",
+            placement: "left",
+        }
+    ],
+    debug: true, // you may wish to turn on debug for the first run through
+    framework: 'bootstrap4' // set Tourist to use BS4 compatibility
+});
+
+
+// Clear Local Storage for Bootstrap Tour
+
+localStorage.clear();
+
+
+tour.init();
+
+
+tour.start();
